ac7dfd840fc9ab6f4bf2dcc8ea87bf9873fafd69
Souraevshing <isauravshing@gmail.com>
2024-12-13 22:33:36 +0530

    feat: add global types and mock date for orders and products

---
+++ src/utils/orders.ts
@@ -0,0 +1,108 @@
+import { Order } from "../../types/global";
+
+export const ORDERS: Order[] = [
+  {
+    id: 1,
+    item: "Order 1",
+    details: "Details about order 1",
+    status: "pending",
+    slug: "order-1",
+    createdAt: "2024-07-01",
+    items: [
+      {
+        id: 1,
+        title: "Product 1",
+        slug: "product-1",
+        image: ["url1"],
+        price: 10.0,
+        heroImage: require("../assets/images/i-phone-1.jpg"),
+      },
+      {
+        id: 2,
+        title: "Product 2",
+        slug: "product-2",
+        image: ["url2"],
+        price: 20.0,
+        heroImage: require("../assets/images/head-set-1.jpg"),
+      },
+    ],
+  },
+  {
+    id: 2,
+    item: "Order 2",
+    details: "Details about order 2",
+    status: "completed",
+    slug: "order-2",
+    createdAt: "2024-07-02",
+    items: [
+      {
+        id: 3,
+        title: "Product 3",
+        slug: "product-3",
+        image: ["url3"],
+        price: 30.0,
+        heroImage: require("../assets/images/head-set-1.jpg"),
+      },
+      {
+        id: 4,
+        title: "Product 4",
+        slug: "product-4",
+        image: ["url4"],
+        price: 40.0,
+        heroImage: require("../assets/images/head-set-1.jpg"),
+      },
+    ],
+  },
+  {
+    id: 3,
+    item: "Order 3",
+    details: "Details about order 3",
+    status: "shipped",
+    slug: "order-3",
+    createdAt: "2024-07-03",
+    items: [
+      {
+        id: 5,
+        title: "Product 5",
+        slug: "product-5",
+        image: ["url5"],
+        price: 50.0,
+        heroImage: require("../assets/images/head-set-1.jpg"),
+      },
+      {
+        id: 6,
+        title: "Product 6",
+        slug: "product-6",
+        image: ["url6"],
+        price: 60.0,
+        heroImage: require("../assets/images/head-set-1.jpg"),
+      },
+    ],
+  },
+  {
+    id: 4,
+    item: "Order 4",
+    details: "Details about order 4",
+    status: "in-transit",
+    slug: "order-4",
+    createdAt: "2024-07-04",
+    items: [
+      {
+        id: 7,
+        title: "Product 7",
+        slug: "product-7",
+        image: ["url7"],
+        price: 70.0,
+        heroImage: require("../assets/images/head-set-1.jpg"),
+      },
+      {
+        id: 8,
+        title: "Product 8",
+        slug: "product-8",
+        image: ["url8"],
+        price: 80.0,
+        heroImage: require("../assets/images/head-set-1.jpg"),
+      },
+    ],
+  },
+];
---
+++ types/global.d.ts
@@ -0,0 +1,29 @@
+import { ImageSourcePropType } from "react-native";
+
+export declare type Category = {
+  name: string;
+  image: string;
+  slug: string;
+  products: Product[];
+};
+
+export type Order = {
+  id: number;
+  slug: string;
+  item: string;
+  details: string;
+  status: "pending" | "completed" | "shipped" | "in-transit";
+  createdAt: string;
+  items: Product[];
+};
+
+export type Product = {
+  id: number;
+  title: string;
+  slug: string;
+  image: (ImageSourcePropType | string)[];
+  price: number;
+  heroImage: ImageSourcePropType;
+  category?: Omit<Category, "products">;
+  quantity?: number;
+};
